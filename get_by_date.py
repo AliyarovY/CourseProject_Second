02e0fdba5abb73d5c631fe3c4c3129d50879675b
CodePredatorr <yaliyarov@mail.ru>
2022-12-02 12:23:22 +0300

    get_by_date Upgrade

--- get_by_date.py
+++ get_by_date.py
@@ -2,41 +2,74 @@ from math import ceil
 from re import search
 
 
-def get_by_date(date: str, name: str, filename='dump.csv') -> None:
-    is_correct_date = search(r'\d{4}(-\d{2}){2}', date).group(0)
+def get_by_date(name: str, date=None, filename='dump.csv') -> None:
+    name = name.upper()
+    date = date or name
+    if date is name:
+        is_correct_date = search(r'[A-Z]+', date).group(0)
+    else:
+        is_correct_date = search(r'\d{4}(-\d{2}){2}', date).group(0)
+
     assert is_correct_date == date
     is_correct_name = search(r'[A-Z]+', name).group(0)
     assert is_correct_name == name
 
     with open('all_stocks_5yr.csv') as ff, open(filename, 'a') as w_ff:
         lines = ff.readlines()
-        int_date = int(''.join(date.split('-')))
         lnn = len(lines)
         cn, r, l = lnn // 2, lnn - 1, 1
 
         # MAIN SEARCH
-        while lines[cn].split(',')[0] != date:
+        while True:
 
-            cn_date = int(''.join(lines[cn].split(',')[0].split('-')))
+            line = lines[cn]
+            cn_name = line.split(',')[-1].strip()
 
-            if int_date < cn_date:
+            if name == cn_name:
+                break
+
+            if name < cn_name:
                 r, cn = cn, cn // 2
 
-            if int_date > cn_date:
+            if name > cn_name:
                 l, cn = cn, cn + ceil((r - cn) / 2)
 
-        if all([x in lines[cn] for x in [date, name]]):
-            w_ff.write(lines[cn])
+        if date in line and cn_name == name:
+            w_ff.write(line)
 
         # search name ahead
         for i in range(cn + 1, len(lines)):
-            if date not in lines[i]:
+            line = lines[i]
+
+            if name not in line:
                 break
-            if name in (res := lines[i]):
-                w_ff.write(res)
+
+            if name != date:
+                if date in line:
+                    w_ff.write(lines[i])
+
+            if date == name:
+                if name == lines[i].split(',')[-1].strip():
+                    w_ff.write(lines[i])
+                else:
+                    break
+
         # search name from behind
         for i in range(cn - 1, -1, -1):
-            if date not in lines[i]:
+            line = lines[i]
+
+            if name not in line:
                 break
-            if name in (res := lines[i]):
-                w_ff.write(res)
+
+            if name != date:
+                if date in line:
+                    w_ff.write(lines[i])
+
+            if date == name:
+                if name == lines[i].split(',')[-1].strip():
+                    w_ff.write(lines[i])
+                else:
+                    break
+
+
+get_by_date('RRC')
